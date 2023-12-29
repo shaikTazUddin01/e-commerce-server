1fa4137c3e7a3f29bba04bc1bbf78ede87607687
sheikhtazuddin <tazahmedsoft@gmail.com>
2023-12-29 15:10:37 +0600

    post category

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 //export reqire library
 const express = require('express')
 const cors = require('cors')
-
+// require datadase
+const { MongoClient, ServerApiVersion } = require('mongodb');
+//dotenv
+require('dotenv').config()
 //create a instance 
 const app = express()
 //port
@@ -10,6 +13,50 @@ const port = process.env.PORT || 5000
 //middleware
 app.use(express.json())
 app.use(cors())
+
+
+// db uri
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.25fgudl.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        const CategoryCollection = client.db('E-commerce').collection('categoryList')
+
+//post category
+        app.post('/createCategoryCollection', async(req, res) => {
+            const category = req.body;
+            // console.log(category)
+            const result =await CategoryCollection.insertOne(category);
+            console.log(result)
+            res.send(result)
+
+        })
+
+
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
 
 //run server
 app.get('/', (req, res) => {
